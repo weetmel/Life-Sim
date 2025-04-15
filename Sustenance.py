08ff1f85a42f4a921d3ab7d4f5f0e812c69f731b
weetmel <weetmch@gmail.com>
2025-04-14 22:50:05 -0700

    Reproduction
    Reproduction done? I have to either make sure maturity is working properly or tweak some ease of life settings. Something must be done to control population booms

--- Sustenance.py
+++ Sustenance.py
@@ -4,16 +4,16 @@ import random
 
 def generate_water(width, height):
     water_sources = []
-    for i in range(random.randint(1,3)):
+    for i in range(random.randint(2,4)):
         pos = (random.randint(30, width - 30), random.randint(30, height - 30))
-        size = random.randint(30,100)
+        size = random.randint(75,150)
         color = pygame.Color("skyblue")
         water_sources.append((pos, size, color, 'water'))
     return water_sources
 
 def generate_food(width, height):
     food_sources = []
-    for i in range(random.randint(3,9)):
+    for i in range(random.randint(5,9)):
         pos = (random.randint(10, width - 10), random.randint(10, height - 10))
         size = random.randint(10,30)
         color = pygame.Color((255, 67, 0))
